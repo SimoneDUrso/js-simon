40d95f9f2d01eaeddf3d8cffad1c227524328a39
SimoneDUrso <simone.durso98@gmail.com>
2024-07-09 16:00:26 +0200

    [UPD] Created function to generate random numbers

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Funzione per la generazione dei 5 numeri casuali
+function randomNumbers() {
+    let randomNumbers = [];
+
+    while (randomNumbers.length < 5) {
+        let num = Math.floor(Math.random() * 100 + 1)
+
+        if (!randomNumbers.includes(num)) {
+            randomNumbers.push(num);
+        }
+    }
+    return randomNumbers;
+}
+
+
+
+
